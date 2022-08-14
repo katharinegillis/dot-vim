a05802d60505f6f60e2b17c8ce0f79f5c8d7b8b3
Katharine Cordes <gillis.katharine@gmail.com>
2022-08-13 23:23:39 -0600

    refactor: remove non-Linux logic from package

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-SYSTEM=$3
-
-if [ "$SYSTEM" == "mac" ]; then
-    brew uninstall vim
-else
-    sudo apt-get remove vim -y
-fi
+sudo apt-get remove vim -y
--- update.sh
+++ update.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-SYSTEM=$3
-
-if [ "$SYSTEM" == "mac" ]; then
-    brew upgrade vim
-else
-    sudo apt-get install vim -y
-fi
+sudo apt-get install vim -y
